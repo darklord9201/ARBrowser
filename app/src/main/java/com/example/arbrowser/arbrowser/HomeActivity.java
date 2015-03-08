9037eedf46e1062ede428c1d70521f945b89737c
[Saugat Chaudhary] <darklord9201@live.com>
2015-03-08 21:22:14 +0545

    Layout Created

--- app/src/main/java/com/example/arbrowser/arbrowser/HomeActivity.java
+++ app/src/main/java/com/example/arbrowser/arbrowser/HomeActivity.java
@@ -1,17 +1,18 @@
 package com.example.arbrowser.arbrowser;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class HomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.homescreen);
     }
 
 
